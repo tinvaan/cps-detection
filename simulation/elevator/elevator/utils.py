2f735bce3035cd3182918e2ec07042eef0a39f7c
Harish Navnit <harishnavnit@gmail.com>
2024-08-12 16:00:46 +0800

    Fix grouping logic

--- simulation/elevator/elevator/utils.py
+++ simulation/elevator/elevator/utils.py
@@ -7,8 +7,11 @@ def merge(intervals):
     m = [intervals[0]]
     for curr in intervals[1:]:
         last = m.pop()
-        if last[1] in range(curr[0], curr[1] + 1):
-            m.append((last[0], curr[1]))
+
+        if (last[1] in range(curr[0], curr[1] + 1) or
+            curr[1] in range(last[0], last[1] + 1)
+        ):
+            m.append((min(last[0], curr[0]), max(last[1], curr[1])))
         else:
             m.extend([last, curr])
     return m
@@ -29,4 +32,5 @@ def group(ts):
 if __name__ == '__main__':
     assert merge([(5, 19), (7, 19)]) == [(5, 19)]
     assert merge([(2, 17), (3, 19), (15, 19)]) == [(2, 19)]
+    assert merge([(1, 19), (4, 16), (5, 19), (13, 19)]) == [(1, 19)]
     assert merge([(0, 12), (1, 17), (1, 19), (1, 19), (11, 19)]) == [(0, 19)]
