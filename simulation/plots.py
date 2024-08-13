3fa8f58fb3a2d9ecc12023c717b07ad12174daef
Harish Navnit <harishnavnit@gmail.com>
2024-08-13 16:55:34 +0800

    Show plots

--- simulation/plots.py
+++ simulation/plots.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 from simulation.elevator.runtime import Config
 
 
-def draw(frame, runs, dst=None):
+def draw(frame, dst=None):
+    runs = frame.readings.tolist().pop()
     category = frame.category.unique().tolist().pop()
     temps, maxTemp = [status['temp'] for status in runs], max([status['MAX_TEMP'] for status in runs])
     weights, maxWeight = [status['weight'] for status in runs], max([status['MAX_WEIGHT'] for status in runs])
 
     fig, axs = plt.subplots(2, figsize=(12, 12))
     axs[0].set_title(f"Raw sensor measurements (Attack type: {category})")
+    # axs[0].plot(frame.change_points.tolist().pop(), linestyle="-", linewidth=2.0, label="Change Points")
+    # axs[0].plot(frame.attack_points.tolist().pop(), linestyle="-", linewidth=2.0, label="Attack Points")
+
     if category == "BUTTON_ATTACK":
         axs[0].plot([1 if status['currentLevel'] == 1 else 0 for status in runs],
                     linestyle="-", linewidth=1, label="CurrentLevel1")
@@ -25,6 +29,7 @@ def draw(frame, runs, dst=None):
         axs[0].plot(temps, linestyle="-", linewidth=0.8, label="Temperature")
 
     elif category == "ATTACK_MAX_WEIGHT":
+        axs[0].set_title(f"Elevator Load (Attack type: {category})")
         axs[0].plot(maxWeight, linestyle="-", linewidth=5, label="MAX_WEIGHT")
         axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
 
@@ -36,15 +41,17 @@ def draw(frame, runs, dst=None):
         axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
     axs[0].legend()
 
-    axs[1].plot([status["moving"] for status in runs], linestyle="-", linewidth=0.8, label="moving")
-    axs[1].plot([status["fire_alarm"] for status in runs], linestyle="-", linewidth=5, label="fire_alarm")
-    axs[1].plot([status["overweight_alarm"] for status in runs], linestyle="-", linewidth=5, label="overweight_alarm")
-    axs[1].set_title("Fire Alarm, Elevator Load and Moving Status")
+    # axs[1].plot(frame.change_points.tolist().pop(), linestyle="-", linewidth=2.0, label="Change Points")
+    # axs[1].plot(frame.attack_points.tolist().pop(), linestyle="-", linewidth=2.0, label="Attack Points")
+    axs[1].plot([status["moving"] for status in runs], linestyle="-", linewidth=0.8, label="Moving")
+    axs[1].plot([status["fire_alarm"] for status in runs], linestyle="-", linewidth=5, label="Fire Alarm")
+    axs[1].plot([status["overweight_alarm"] for status in runs], linestyle="-", linewidth=5, label="Load Alarm")
+    axs[1].set_title("Fire alarm, Load alarm, Elevator motion")
     axs[1].legend()
 
     plt.tight_layout()
     if Config.SHOW_PLOTS:
-        plt.show()
+        plt.show(block=True)
 
     if dst and Config.SAVE_PLOTS:
         num = len([f for f in os.listdir(dst) if os.path.isfile(os.path.join(dst, f))]) + 1
