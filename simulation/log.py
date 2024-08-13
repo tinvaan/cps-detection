24faad089e1a02ddddbf0353514eb921f4ea59e1
Harish Navnit <harishnavnit@gmail.com>
2024-08-13 19:48:43 +0800

    Use the frame with highest attack to detect overlap

--- simulation/log.py
+++ simulation/log.py
@@ -49,10 +49,17 @@ class ChangeWriter:
         self.changes.to_csv(fname, mode=mode, index=False, header=not path.exists(fname))
 
         if Config.SHOW_PLOTS or Config.SAVE_PLOTS:
-            df = self.changes.loc[self.changes['detection_effectiveness'] ==
-                                  self.changes['detection_effectiveness'].max()]
-            df = df.loc[df['false_alarm_rate'] == df['false_alarm_rate'].min()]\
-                    .sort_values(by='attacks', ascending=False).iloc[:1]
+            frames = self.changes.loc[self.changes['detection_effectiveness'] ==
+                                      self.changes['detection_effectiveness'].max()]
+            frames = frames.loc[frames['false_alarm_rate'] == frames['false_alarm_rate'].min()]\
+                            .sort_values(by='attacks', ascending=False)
+
+            # Find the frame with highest attack and detection overlap
+            for idx, df in frames.iterrows():
+                for cp in df.change_points:
+                    if all([cp in range(pts[0], pts[1]) for pts in df.attack_points]):
+                        break
+
             return plots.draw(df)
 
     def process(self, summary):
--- simulation/plots.py
+++ simulation/plots.py
@@ -6,44 +6,44 @@ from simulation.elevator.runtime import Config
 
 
 def draw(frame, dst=None):
-    runs = frame.readings.tolist().pop()
-    category = frame.category.unique().tolist().pop()
-    temps, maxTemp = [status['temp'] for status in runs], [status['MAX_TEMP'] for status in runs]
-    weights, maxWeight = [status['weight'] for status in runs], [status['MAX_WEIGHT'] for status in runs]
+    temps  = [status['temp'] for status in frame.readings]
+    weights = [status['weight'] for status in frame.readings]
+    maxTemp = [status['MAX_TEMP'] for status in frame.readings]
+    maxWeight = [status['MAX_WEIGHT'] for status in frame.readings]
 
     fig, axs = plt.subplots(2, figsize=(12, 12))
-    axs[0].set_title(f"Raw sensor measurements (Attack type: {category})")
+    axs[0].set_title(f"Raw sensor measurements (Attack type: {frame.category})")
 
-    detections = frame.change_points.tolist().pop()
-    d = plt.axvspan(min(detections), max(detections), alpha=0.25, color='yellow', label=f'Detect')
+    d = plt.axvspan(min(frame.change_points), max(frame.change_points),
+                    alpha=0.25, color='yellow', label=f'Detect')
     plt.annotate(
         xy=d.get_center(),
         text=f'''
-        Detection Effectiveness = {frame.detection_effectiveness.tolist().pop()}%
-        False Alarm Rate = {frame.false_alarm_rate.tolist().pop()}%
+        Detection Effectiveness = {frame.detection_effectiveness}%
+        False Alarm Rate = {frame.false_alarm_rate}%
         '''
     )
 
-    for idx, attacks in enumerate(frame.attack_points.tolist().pop()):
+    for idx, attacks in enumerate(frame.attack_points):
         if idx > 0:
             plt.axvspan(min(attacks), max(attacks), alpha=0.1, color='red')
         else:
             plt.axvspan(min(attacks), max(attacks), alpha=0.1, color='red', label=f'Attack')
 
-    if category == "BUTTON_ATTACK":
-        axs[0].plot([1 if status['currentLevel'] == 1 else 0 for status in runs],
+    if frame.category == "BUTTON_ATTACK":
+        axs[0].plot([1 if status['currentLevel'] == 1 else 0 for status in frame.readings],
                     linestyle="-", linewidth=1, label="CurrentLevel1")
-        axs[0].plot([status['ButtonLevel1'] for status in runs],
+        axs[0].plot([status['ButtonLevel1'] for status in frame.readings],
                     linestyle="-", linewidth=0.8, label="ButtonLevel1")
-        axs[0].plot([status['ButtonLevel2'] for status in runs],
+        axs[0].plot([status['ButtonLevel2'] for status in frame.readings],
                     linestyle="-", linewidth=0.8, label="ButtonLevel2")
 
-    elif category == "ATTACK_MAX_TEMP":
+    elif frame.category == "ATTACK_MAX_TEMP":
         axs[0].plot(maxTemp, linestyle="-", linewidth=5, label="MAX_TEMP")
         axs[0].plot(temps, linestyle="-", linewidth=0.8, label="Temperature")
 
-    elif category == "ATTACK_MAX_WEIGHT":
-        axs[0].set_title(f"Elevator Load (Attack type: {category})")
+    elif frame.category == "ATTACK_MAX_WEIGHT":
+        axs[0].set_title(f"Elevator Load (Attack type: {frame.category})")
         axs[0].plot(maxWeight, linestyle="-", linewidth=5, label="MAX_WEIGHT")
         axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
 
@@ -55,9 +55,9 @@ def draw(frame, dst=None):
         axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
     axs[0].legend()
 
-    axs[1].plot([status["moving"] for status in runs], linestyle="-", linewidth=0.8, label="Moving")
-    axs[1].plot([status["fire_alarm"] for status in runs], linestyle="-", linewidth=5, label="Fire Alarm")
-    axs[1].plot([status["overweight_alarm"] for status in runs], linestyle="-", linewidth=5, label="Load Alarm")
+    axs[1].plot([status["moving"] for status in frame.readings], linestyle="-", linewidth=0.8, label="Moving")
+    axs[1].plot([status["fire_alarm"] for status in frame.readings], linestyle="-", linewidth=5, label="Fire Alarm")
+    axs[1].plot([status["overweight_alarm"] for status in frame.readings], linestyle="-", linewidth=5, label="Load Alarm")
     axs[1].set_title("Fire alarm, Load alarm, Elevator motion")
     axs[1].legend()
 
