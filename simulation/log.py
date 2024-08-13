69d32bb9c81bda05927228f82df8d700c91e6309
Harish Navnit <harishnavnit@gmail.com>
2024-08-13 16:55:34 +0800

    Show plots

--- simulation/log.py
+++ simulation/log.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 from os import path
+from tqdm import tqdm
 
 from simulation import plots
 from simulation.elevator import runtime
@@ -34,8 +35,8 @@ class ChangeWriter:
 
         rs = self.changes.loc[self.changes.category == category]
         if best:
-            rs = rs[rs['detection_effectiveness'] == rs['detection_effectiveness'].max()]
-            rs = rs[rs['false_alarm_rate'] == rs['false_alarm_rate'].min()]
+            rs = rs.loc[rs['detection_effectiveness'] == rs['detection_effectiveness'].max()]
+            rs = rs.loc[rs['false_alarm_rate'] == rs['false_alarm_rate'].min()]
 
         return rs.loc[rs['detection_effectiveness'] > Config.MIN_DETECTION_EFFECTIVENESS]\
                  .loc[rs['false_alarm_rate'] < Config.MAX_FALSE_ALARM_RATE]\
@@ -46,6 +47,13 @@ class ChangeWriter:
         fname = path.join(runs, "results.csv")
         mode = "a" if (path.exists(fname) and path.getsize(fname) != 0) else "w"
         self.changes.to_csv(fname, mode=mode, index=False, header=not path.exists(fname))
+
+        if Config.SHOW_PLOTS or Config.SAVE_PLOTS:
+            df = self.changes.loc[self.changes['detection_effectiveness'] ==
+                                  self.changes['detection_effectiveness'].max()]
+            df = df.loc[df['false_alarm_rate'] == df['false_alarm_rate'].min()]\
+                    .sort_values(by='attacks', ascending=False).iloc[:1]
+            plots.draw(df)
 
     def process(self, summary):
         for idx, record in enumerate(summary):
