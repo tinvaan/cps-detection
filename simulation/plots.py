2e9cb6a9b1380e8b25adf3d811c36f1eba0247fd
Harish Navnit <harishnavnit@gmail.com>
2024-08-13 15:02:20 +0800

    [WIP] Fix plolts drawer

--- simulation/plots.py
+++ simulation/plots.py
@@ -5,66 +5,48 @@ import matplotlib.pyplot as plt
 from simulation.elevator.runtime import Config
 
 
-def draw(MAX_TEMP, MAX_WEIGHT, sensor_measurements, actuators_status, title=("NONE", False), detection_status=None):
+def draw(frame, runs, dst=None):
+    category = frame.category.unique().tolist().pop()
+    temps, maxTemp = [status['temp'] for status in runs], max([status['MAX_TEMP'] for status in runs])
+    weights, maxWeight = [status['weight'] for status in runs], max([status['MAX_WEIGHT'] for status in runs])
+
     fig, axs = plt.subplots(2, figsize=(12, 12))
-    axs[0].set_title(f"Raw sensor measurements (Attack type: {title[0]})")
+    axs[0].set_title(f"Raw sensor measurements (Attack type: {category})")
+    if category == "BUTTON_ATTACK":
+        axs[0].plot([1 if status['currentLevel'] == 1 else 0 for status in runs],
+                    linestyle="-", linewidth=1, label="CurrentLevel1")
+        axs[0].plot([status['ButtonLevel1'] for status in runs],
+                    linestyle="-", linewidth=0.8, label="ButtonLevel1")
+        axs[0].plot([status['ButtonLevel2'] for status in runs],
+                    linestyle="-", linewidth=0.8, label="ButtonLevel2")
 
-    if title[0] == "ATTACK_MAX_TEMP":
-        axs[0].plot(MAX_TEMP, linestyle="-", linewidth=5, label="MAX_TEMP")
-        axs[0].plot(sensor_measurements, linestyle="-", linewidth=0.8, label="Temp")
+    elif category == "ATTACK_MAX_TEMP":
+        axs[0].plot(maxTemp, linestyle="-", linewidth=5, label="MAX_TEMP")
+        axs[0].plot(temps, linestyle="-", linewidth=0.8, label="Temperature")
 
-    elif title[0] == "ATTACK_MAX_WEIGHT":
-        weights = [status['weight'] for status in actuators_status]
-        max_weights = [status['MAX_WEIGHT'] for status in actuators_status]
-        axs[0].plot(MAX_WEIGHT, linestyle="-", linewidth=5, label="MAX_WEIGHT")
-        axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Weight")
+    elif category == "ATTACK_MAX_WEIGHT":
+        axs[0].plot(maxWeight, linestyle="-", linewidth=5, label="MAX_WEIGHT")
+        axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
 
-    elif title[0] == "BUTTON_ATTACK":
-        button_level1 = [status['ButtonLevel1'] for status in actuators_status]
-        button_level2 = [status['ButtonLevel2'] for status in actuators_status]
-        current_level1 = [1 if status['currentLevel'] == 1 else 0 for status in actuators_status]
-        axs[0].plot(current_level1, linestyle="-", linewidth=1, label="CurrentLevel1")
-        axs[0].plot(button_level1, linestyle="-", linewidth=0.8, label="ButtonLevel1")
-        axs[0].plot(button_level2, linestyle="-", linewidth=0.8, label="ButtonLevel2")
+    else:   # BIAS, SURGE, RANDOM, etc...
+        axs[0].plot(maxTemp, linestyle="-", linewidth=5, label="MAX_TEMP")
+        axs[0].plot(maxWeight, linestyle="-", linewidth=5, label="MAX_WEIGHT")
 
-    elif title[0]=="BIAS":
-        axs[0].plot(MAX_TEMP, linestyle="-", linewidth=5, label="thresholds")
-        axs[0].plot(sensor_measurements, linestyle="-", linewidth=0.8, label="actual")
-
-    elif title[0]=="SURGE":
-        axs[0].plot(MAX_TEMP, linestyle="-", linewidth=5, label="thresholds")
-        axs[0].plot(sensor_measurements, linestyle="-", linewidth=0.8, label="actual")
-
-    elif title[0]=="RANDOM":
-        axs[0].plot(MAX_TEMP, linestyle="-", linewidth=5, label="thresholds")
-        axs[0].plot(sensor_measurements, linestyle="-", linewidth=0.8, label="actual")
-
-    else:
-        axs[0].plot(MAX_TEMP, linestyle="-", linewidth=5, label="thresholds")
-        axs[0].plot(sensor_measurements, linestyle="-", linewidth=0.8, label="actual")
-
-    if detection_status:
-        attack_indices = [i for i, status in enumerate(detection_status) if status == "attack"]
-        axs[0].scatter(attack_indices, [sensor_measurements[i] for i in attack_indices], color='red', label='Attack detected', zorder=5)
-
+        axs[0].plot(temps, linestyle="-", linewidth=0.8, label="Temperature")
+        axs[0].plot(weights, linestyle="-", linewidth=0.8, label="Elevator load")
     axs[0].legend()
 
-    fire_alarm_status = [status["fire_alarm"] for status in actuators_status]
-    moving_status = [status["moving"] for status in actuators_status]
-    overweight_alarm = [status["overweight_alarm"] for status in actuators_status]
-    axs[1].plot(fire_alarm_status, linestyle="-", linewidth=5, label="fire_alarm")
-    axs[1].plot(overweight_alarm, linestyle="-", linewidth=5, label="overweight_alarm")
-    axs[1].plot(moving_status, linestyle="-", linewidth=0.8, label="moving")
-    axs[1].set_title("Fire Alarm, Overweight Alarm, and Moving Status")
+    axs[1].plot([status["moving"] for status in runs], linestyle="-", linewidth=0.8, label="moving")
+    axs[1].plot([status["fire_alarm"] for status in runs], linestyle="-", linewidth=5, label="fire_alarm")
+    axs[1].plot([status["overweight_alarm"] for status in runs], linestyle="-", linewidth=5, label="overweight_alarm")
+    axs[1].set_title("Fire Alarm, Elevator Load and Moving Status")
     axs[1].legend()
 
     plt.tight_layout()
-
     if Config.SHOW_PLOTS:
-        plt.show(block=False)
+        plt.show()
 
-    if Config.SAVE_PLOTS:
-        filepath = f"runs/ATTACK_TYPE_{title[0]}"
-        num = len([f for f in os.listdir(filepath) if os.path.isfile(os.path.join(filepath, f))]) + 1
-        fig.savefig(f"{filepath}/{num}.png")
+    if dst and Config.SAVE_PLOTS:
+        num = len([f for f in os.listdir(dst) if os.path.isfile(os.path.join(dst, f))]) + 1
+        fig.savefig(f"{dst}/{num}.png")
         plt.close()
